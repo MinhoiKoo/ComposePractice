2785d8e9d978cac3564e8f9741c2e816e242eacf
MinhoiKoo <minlove2013@gmail.com>
2024-06-04 01:15:23 +0900

    initial commit

---
+++ app/src/main/java/com/minhoi/composepractice/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.minhoi.composepractice
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.minhoi.composepractice.ui.theme.ComposePracticeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposePracticeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Column {
+                        Text("hello")
+                        Text("world")
+                    }
+                }
+            }
+        }
+    }
+}
